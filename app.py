563b309f32b05d0d9fb9596f5a72ce81a4b49fcf
Jacek J. Łakis <mlody3k@gmail.com>
2022-05-12 21:13:41 +0200

    Front improvements

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from datetime import datetime
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from apscheduler.schedulers.background import BackgroundScheduler
 from tuya import get_device
+
+#from state import state
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -17,11 +19,16 @@ message = "Z jakiegoś powodu nie możemy określić statusu otwarcia KDP"
 latest_online_status = False
 latest_check_time = "Nigdy"
 latest_update_time = "jakiegoś czasu"
+visitors_today = {"day": 0, "visitors": {}}
+latest_api_response = {}
+sessions = [] # {"created_at": ..., "pic": }
 
-@sched.scheduled_job('interval', seconds=120)
+@sched.scheduled_job('interval', seconds=5)
 def scrap():
     print("Scrap job")
-    global latest_update_time, status, message, online, latest_check_time, latest_online_status
+    global latest_update_time, status, message, online, latest_check_time, latest_online_status, latest_api_response
+    
+    latest_check_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     try:
         device_data = get_device()
@@ -32,6 +39,7 @@ def scrap():
         return
     
     print("  Device data retrieved")
+    latest_api_response = device_data
 
     try:
         online = device_data['result']['online']
@@ -58,12 +66,24 @@ def scrap():
         message = f"KDP niestety jest zamknięty. Żarówka nad barem jest zgaszona od {latest_update_time}."
         print("  We're offline")
 
-    latest_check_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(f"  We checked at {latest_check_time}")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
+    global visitors_today
+
+    epoch_days = (datetime.utcnow() - datetime(1970,1,1)).days
+    ip = request.client.host
+
+    if epoch_days == visitors_today['day']:
+        visitors_today['visitors'].add(ip)
+    else:
+        visitors_today['day'] = epoch_days
+        visitors_today['visitors'] = {ip}
+
+    number_of_visitors_today = len(visitors_today['visitors'])
+
     image_url = "static/unknown_sign.png"
 
     if status == "Online":
@@ -76,8 +96,22 @@ async def index(request: Request):
             "request": request,
             "picture_link": image_url,
             "message": message,
-            "latest_check_time": latest_check_time
+            "latest_check_time": latest_check_time,
+            "number_of_visitors_today": number_of_visitors_today,
+            "client_ip": ip
         }
     )
 
+@app.get("/latest_response.json")
+async def index():
+    return latest_api_response
+
+@app.get("/woz")
+async def index(lid: str): # Albo sprawdzmy czy jest w kuki
+    if not lid:
+        return RedirectResponse()
+    # If lid=asdasdasd w passie jest inny niż to co znajdziemy w arrayu z sesjami 
+    return latest_api_response
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/static", StaticFiles(directory="static"), name="static")
--- config.py
+++ config.py
@@ -7,3 +7,22 @@ tuya_device_id = os.getenv("TUYA_DEVICE_ID", "bfa2136abce036da4cw7zo") # bar pra
 tuya_api_url = "https://openapi.tuyaeu.com"
 tuya_token_path = "/v1.0/token?grant_type=1"
 tuya_device_path = f"/v1.0/devices/{tuya_device_id}"
+
+
+oauth2_token_url = 'https://graph.facebook.com/v6.0/oauth/access_token'
+oauth2_user_info_url = 'https://graph.facebook.com/v6.0/me'
+oauth2_client_id = os.getenv('OAUTH2_CLIENT_ID', 'oauth2_client_id')
+oauth2_client_secret = os.getenv('OAUTH2_CLIENT_SECRET', 'sekret')
+oauth2_redirect_uri = os.getenv('OAUTH2_REDIRECT_URI', 'oauth2_redirect_uri')
+
+oauth2_token_fetch_url = oauth2_token_url+'?client_id='+oauth2_client_id + \
+    '&client_secret='+oauth2_client_secret+'&redirect_uri='+oauth2_redirect_uri
+oauth2_app_creds_fetch_url = "{}?client_id={}&client_secret={}&grant_type=client_credentials".format(
+    "https://graph.facebook.com/oauth/access_token", oauth2_client_id, oauth2_client_secret
+)
+
+
+allowed_userids = [
+    100001820504411, # Stachu
+    1401053905 # Ja
+]
