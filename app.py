a4c46d9908b8e6df6f351b17b3c11beb89956ce7
Jacek J. Łakis <mlody3k@gmail.com>
2022-05-12 21:26:45 +0200

    state sorted in classes

--- app.py
+++ app.py
@@ -6,97 +6,88 @@ from fastapi.templating import Jinja2Templates
 from apscheduler.schedulers.background import BackgroundScheduler
 from tuya import get_device
 
-#from state import state
+from state import state
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 sched = BackgroundScheduler(timezone="UTC")
 sched.start()
 
-# Global status
-status = "Nieznany"
-message = "Z jakiegoś powodu nie możemy określić statusu otwarcia KDP"
-latest_online_status = False
-latest_check_time = "Nigdy"
-latest_update_time = "jakiegoś czasu"
-visitors_today = {"day": 0, "visitors": {}}
-latest_api_response = {}
-sessions = [] # {"created_at": ..., "pic": }
-
 @sched.scheduled_job('interval', seconds=5)
 def scrap():
-    print("Scrap job")
-    global latest_update_time, status, message, online, latest_check_time, latest_online_status, latest_api_response
+    current_state = state.get_state()
     
-    latest_check_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    current_state['latest_check_time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     try:
         device_data = get_device()
     except Exception as e:
-        status = "Nieznany"
-        message = f"Wystąpił błąd podczas pobierania danych diwajsa: {e}"
+        current_state['status'] = "Nieznany"
+        current_state['message'] = f"Wystąpił błąd podczas pobierania danych diwajsa: {e}"
         print(f"  Device data retrieval failed: {type(e)}: {e.args}")
         return
     
     print("  Device data retrieved")
-    latest_api_response = device_data
+    current_state['latest_api_response'] = device_data
 
     try:
         online = device_data['result']['online']
-        if online != latest_online_status and status != "Nieznany":
-            latest_update_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print(f"  Status updated at {latest_update_time}")
+        if online != current_state['latest_online_status'] and current_state['status'] != "Nieznany":
+            current_state['latest_update_time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"  Status updated at {current_state['latest_update_time']}")
         else:
             print("  Status remained")
         
-        latest_online_status = online
+        current_state['latest_online_status'] = online
 
     except Exception as e:
-        status = "Nieznany"
-        meesage = f"Wystąpił błąd podczas parsowania danych diwajsa: {type(e)}: {e.args}"
+        current_state['status'] = "Nieznany"
+        current_state['meesage'] = f"Wystąpił błąd podczas parsowania danych diwajsa: {type(e)}: {e.args}"
         print(f"  Unable to retrieve data: {type(e)}: {e.args}")
         return
     
     if online:
-        status = "Online"
-        message = f"KDP wygląda na otwarty! Żarówka nad barem świeci się od {latest_update_time}."
+        current_state['status'] = "Online"
+        current_state['message'] = f"KDP wygląda na otwarty! Żarówka nad barem świeci się od {current_state['latest_update_time']}."
         print("  We're online")
     else:
-        status = "Offline"
-        message = f"KDP niestety jest zamknięty. Żarówka nad barem jest zgaszona od {latest_update_time}."
+        current_state['status'] = "Offline"
+        current_state['message'] = f"KDP niestety jest zamknięty. Żarówka nad barem jest zgaszona od {current_state['latest_update_time']}."
         print("  We're offline")
 
-    print(f"  We checked at {latest_check_time}")
+    print(f"  We checked at {current_state['latest_check_time']}")
+
+    state.set_state(current_state)
 
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    global visitors_today
+    current_state = state.get_state()
 
     epoch_days = (datetime.utcnow() - datetime(1970,1,1)).days
     ip = request.client.host
 
-    if epoch_days == visitors_today['day']:
-        visitors_today['visitors'].add(ip)
+    if epoch_days == current_state['visitors_today']['day']:
+        current_state['visitors_today']['visitors'].add(ip)
     else:
-        visitors_today['day'] = epoch_days
-        visitors_today['visitors'] = {ip}
+        current_state['visitors_today']['day'] = epoch_days
+        current_state['visitors_today']['visitors'] = {ip}
 
-    number_of_visitors_today = len(visitors_today['visitors'])
+    number_of_visitors_today = len(current_state['visitors_today']['visitors'])
 
     image_url = "static/unknown_sign.png"
 
-    if status == "Online":
+    if current_state['status'] == "Online":
         image_url = "static/open_sign.png"
-    elif status == "Offline":
+    elif current_state['status'] == "Offline":
         image_url = "static/closed_sign.png"
     
     return templates.TemplateResponse("index.html", 
         {
             "request": request,
             "picture_link": image_url,
-            "message": message,
-            "latest_check_time": latest_check_time,
+            "message": current_state['message'],
+            "latest_check_time": current_state['latest_check_time'],
             "number_of_visitors_today": number_of_visitors_today,
             "client_ip": ip
         }
@@ -104,14 +95,13 @@ async def index(request: Request):
 
 @app.get("/latest_response.json")
 async def index():
-    return latest_api_response
+    return state.get_state()['latest_api_response']
 
 @app.get("/woz")
 async def index(lid: str): # Albo sprawdzmy czy jest w kuki
     if not lid:
         return RedirectResponse()
     # If lid=asdasdasd w passie jest inny niż to co znajdziemy w arrayu z sesjami 
-    return latest_api_response
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/static", StaticFiles(directory="static"), name="static")
---
+++ state.py
@@ -0,0 +1,54 @@
+
+class IsKDPOpenState:
+    def _get_initial_state(self):
+        return {
+            "status": "Nieznany",
+            "message": "Z jakiegoś powodu nie możemy określić statusu otwarcia KDP",
+            "latest_online_status": False,
+            "latest_check_time": "Nigdy",
+            "latest_update_time": "jakiegoś czasu",
+            "visitors_today": {"day": 0, "visitors": {}},
+            "latest_api_response": {},
+            "sessions": [] # {"created_at": ..., "pic": }
+        }
+
+    def _read_state(self):        
+        return self.state
+    
+    def _write_state(self, state):
+        print(f"we set new state {state}")
+        if not hasattr(self, "state"):
+            self.state = {}
+        
+        for s in state.keys():
+            self.state[s] = state[s]
+
+    def get_state(self):
+        state = self._read_state()
+
+        if not state:
+            state = self._get_initial_state()
+            self._write_state(state)
+
+        return state
+
+    def set_state(self, state):
+        return self._write_state(state)
+
+    def __init__(self):
+        self.set_state({})
+
+class IsKDPOpenStateJSONFile(IsKDPOpenState):
+    def __init__(self, filepath):
+        super(IsKDPOpenStateJSONFile).__init__()
+        self.filepath = filepath
+
+    def _read_state(self):
+        return self.state
+    
+    def _write_state(self, state):
+        for s in state.keys():
+            self.state[s] = state[s]
+
+
+state = IsKDPOpenState()
