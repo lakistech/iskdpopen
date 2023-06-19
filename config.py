0331cfa1e1f903277ad651b4ccef239c2cb68771
Jacek J. Łakis <mlody3k@gmail.com>
2023-06-19 23:00:32 +0200

    cleanup & reorg

--- config.py
+++ config.py
@@ -8,20 +8,4 @@ tuya_api_url = "https://openapi.tuyaeu.com"
 tuya_token_path = "/v1.0/token?grant_type=1"
 tuya_device_path = f"/v1.0/devices/{tuya_device_id}"
 
-call_interval = os.getenv("CALL_INTERVAL", "120")
-
-oauth2_token_url = 'https://graph.facebook.com/v6.0/oauth/access_token'
-oauth2_user_info_url = 'https://graph.facebook.com/v6.0/me'
-oauth2_client_id = os.getenv('OAUTH2_CLIENT_ID')
-oauth2_client_secret = os.getenv('OAUTH2_CLIENT_SECRET')
-oauth2_redirect_uri = os.getenv('OAUTH2_REDIRECT_URI')
-
-oauth2_login_url = f"https://facebook.com/v13.0/dialog/oauth?client_id={oauth2_client_id}&redirect_uri={oauth2_redirect_uri}"
-
-oauth2_token_fetch_url = oauth2_token_url+'?client_id='+oauth2_client_id + \
-    '&client_secret='+oauth2_client_secret+'&redirect_uri='+oauth2_redirect_uri
-oauth2_app_creds_fetch_url = "{}?client_id={}&client_secret={}&grant_type=client_credentials".format(
-    "https://graph.facebook.com/oauth/access_token", oauth2_client_id, oauth2_client_secret
-)
-
-allowed_userids = os.getenv("ALLOWED_USERIDS").split(",")
+call_interval = os.getenv("CALL_INTERVAL", "10")
